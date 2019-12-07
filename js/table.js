9322518fa326e19421a2457841ff48cc5c5b4265
tofik-khan <55766480+tofik-khan@users.noreply.github.com>
2019-12-07 00:17:23 -0800

    Fixing syntax errors in js/table.js, Created first testbench

--- js/table.js
+++ js/table.js
@@ -1,17 +1,17 @@
-class Table {
-	const MANUAL = 0; //flag for solid input, user's input goes to userArray
-	const SEMI_AUTO = 1; //flag for semi auto; user can select multiple cells for input
-	const PENCILED = 2;  //flag for penciled input. input will go to pencilArray
+class StonkTable {
+	MANUAL = 0; //flag for solid input, user's input goes to userArray
+	SEMI_AUTO = 1; //flag for semi auto; user can select multiple cells for input
+	PENCILED = 2;  //flag for penciled input. input will go to pencilArray
 
-	const GOOD = 1; //flag for when element of userArray matches the corresponding element of elementArray
-	const BAD = 0; //flag for when element of userArray doesn't match the corresponding element of the elementArray
+	GOOD = 1; //flag for when element of userArray matches the corresponding element of elementArray
+	BAD = 0; //flag for when element of userArray doesn't match the corresponding element of the elementArray
 
 	constructor ()
 	{
-		this.elementArray[81] = makePuzzle(); //creates an unambiguous solved puzzle
-		this.userArray[81]; //stores the user's selected solutions
-		this.pencilArray[81][9]; //stores user's penciled numbers for each cell on table
-		this.inputFlag = MANUAL; //Start in solid input
+		this.elementArray = makePuzzle(); //creates an unambiguous solved puzzle
+		this.userArray = []; //stores the user's selected solutions
+		this.pencilArray = []; //stores user's penciled numbers for each cell on table
+		this.inputFlag = this.MANUAL; //Start in solid input
 
 		this.selectedNumber = 0; //the number user has selected for semiAuto input
 
@@ -22,7 +22,7 @@ class Table {
 		/* Take the elementArray and compare that to the userArray,
 		 it then returns an array with GOOD or BAD depending on
 		 whether user's input matched the elementArray */
-		 var returnArray[];
+		 var returnArray = [];
 		 for (var i = 0; i < this.elementArray.lenght(); i++) {
 		 	if(elementArray[i] == userArray[i]) {
 		 		returnArray.push(GOOD);
