a9cd5ad3414f2d26bab868092e20ba5ee26b5605
tofik-khan <55766480+tofik-khan@users.noreply.github.com>
2019-12-07 22:25:21 -0800

    fixing syntax errors

--- js/user.js
+++ js/user.js
@@ -1,10 +1,8 @@
 class User {
 	constructor (name) {
 		this.username = name;
-		this.difficulty	= getDifficulty();
+		this.difficulty	= this.getDifficulty(); //reset to get difficulty from user
 		this.userTable = new StonkTable ();
-
-		this.startNewGame();
 
 	}
 
@@ -16,15 +14,42 @@ class User {
 
 	startNewGame() {
 		this.userTable.initialTableFill(this.difficulty);
-
+		this.updateHTML_userTable();
 		var score = 0;
 
-		while (this.usertable.checkWin() == false) {
+		while (this.userTable.checkWin() == false) {
 			//keep playing game
+			this.getInput();
 			
 		}
 
 		
+	}
+
+	updateHTML_userTable() {
+		for (var row = 1; row <= 9; row++) {
+			for (var col = 1; col <= 9; col++) {
+
+				var index = (row-1)*9 + (col-1);
+
+				document.getElementById(row + "" + col).value = this.userTable.userArray[index];
+				
+			}
+		}
+	}
+
+	getInput () {
+
+		var coordinate = document.getElementById("location").value;
+		console.log(coordinate);
+		var n = parseInt(coordinate);
+			var row = Math.floor(n/10);
+			var col = n%10;
+
+			var index = (row-1)*9 + (col-1);
+			this.userTable.setInput(document.getElementById("userinput").value, coordinate, index);
+			this.updateHTML_userTable();
+
 	}
 
 }
