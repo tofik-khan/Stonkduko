20c971ee9228af329339f9484bfad2d9f78486c0
tofik-khan <55766480+tofik-khan@users.noreply.github.com>
2019-12-11 11:47:19 -0800

    starting user class again with better clarity...

--- js/user.js
+++ js/user.js
@@ -1,28 +1,14 @@
 class User {
-	constructor (name) {
-		this.username = name;
-		this.difficulty	= this.getDifficulty(); //reset to get difficulty from user
+	constructor (difficulty) {
+		this.difficulty	= difficulty; //reset to get difficulty from user
 		this.userTable = new StonkTable ();
 
 	}
 
-	getDifficulty() {
-		//sets the difficulty for the user
-
-		return 0; //change to return variable difficulty.
-	}
-
 	startNewGame() {
 		this.userTable.initialTableFill(this.difficulty);
-		this.updateHTML_userTable();
 		var score = 0;
-
-		while (this.userTable.checkWin() == false) {
-			//keep playing game
-			this.getInput();
-			
-		}
-
+		this.updateHTML_userTable();
 		
 	}
 
@@ -32,24 +18,11 @@ class User {
 
 				var index = (row-1)*9 + (col-1);
 
-				document.getElementById(row + "" + col).value = this.userTable.userArray[index];
+				console.log(row.toString()+col.toString());
+				document.getElementById(row.toString()+col.toString()).innerHTML = this.userTable.userArray[index];
 				
 			}
 		}
-	}
-
-	getInput () {
-
-		var coordinate = document.getElementById("location").value;
-		console.log(coordinate);
-		var n = parseInt(coordinate);
-			var row = Math.floor(n/10);
-			var col = n%10;
-
-			var index = (row-1)*9 + (col-1);
-			this.userTable.setInput(document.getElementById("userinput").value, coordinate, index);
-			this.updateHTML_userTable();
-
 	}
 
 }
